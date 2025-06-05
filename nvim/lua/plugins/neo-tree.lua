c09875e909ac7909fc321aaf030a1f8cd531d1ff
emirrcaglar <emirrcaglar@gmail.com>
2025-06-05 17:46:20 +0300

    neotree

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,12 +1,40 @@
+-- lazy.nvim
 return {
-	"nvim-neo-tree/neo-tree.nvim",
-	branch = "v3.x",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"nvim-tree/nvim-web-devicons",
-		"MunifTanjim/nui.nvim",
-	},
-	config = function()
-		vim.keymap.set("n", "<C-n>", ":Neotree filesystem reveal float<CR>")
-	end,
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    -- Other dependencies
+    'saifulapm/neotree-file-nesting-config', -- add plugin as dependency. no need any other config or setup call
+  },
+  opts = {
+    -- recommended config for better UI
+    hide_root_node = true,
+    retain_hidden_root_indent = true,
+    filesystem = {
+      filtered_items = {
+        show_hidden_count = false,
+        never_show = {
+          '.DS_Store',
+        },
+      },
+    },
+    default_component_configs = {
+      indent = {
+        with_expanders = true,
+        expander_collapsed = '',
+        expander_expanded = '',
+      },
+    },
+    -- other config
+  },
+  config = function(_, opts)
+    -- Set keymap to toggle Neo-tree
+    vim.keymap.set("n", "<C-n>", ":Neotree toggle<CR>", { silent = true, noremap = true })
+    vim.keymap.set("n", "<Tab>", ":bnext<CR>")
+    vim.keymap.set("n", "<S-Tab>", ":bprev<CR>")
+
+    -- Apply Neo-tree setup with the provided options
+    require("neo-tree").setup(opts)
+  end,
 }
+
